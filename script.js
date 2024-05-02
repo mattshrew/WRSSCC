918ba3e61327d8d3f9ab68c16dcbc06ad74a7f1d
mattshrew <mattshrewy@gmail.com>
2024-05-01 23:07:41 -0400

    finished pairing building

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const uploadBox = document.getElementById('input-box');
+    const inputBox = document.getElementById('input-box');
     const fileInput = document.getElementById('input-file');
   
-    uploadBox.addEventListener('click', function () {
+    inputBox.addEventListener('click', function () {
         fileInput.click();
     });
   
@@ -21,18 +21,18 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     });
   
-    uploadBox.addEventListener('dragover', function (e) {
+    inputBox.addEventListener('dragover', function (e) {
         e.preventDefault();
-        uploadBox.classList.add('dragover');
+        inputBox.classList.add('dragover');
     });
   
-    uploadBox.addEventListener('dragleave', function () {
-        uploadBox.classList.remove('dragover');
+    inputBox.addEventListener('dragleave', function () {
+        inputBox.classList.remove('dragover');
     });
   
-    uploadBox.addEventListener('drop', async function (e) {
+    inputBox.addEventListener('drop', async function (e) {
         e.preventDefault();
-        uploadBox.classList.remove('dragover');
+        inputBox.classList.remove('dragover');
         const file = e.dataTransfer.files[0];
         if (file) {
             console.log('Uploaded file:', file.name);
@@ -48,7 +48,59 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 });
 
-function buildPairings(pairings) {
-    console.log('building...')
-    // do stuff
+function buildPairings(raw) {
+    const inputContainer = document.getElementById('input-container');
+    const pairingsContainer = document.getElementById('pairings-container');
+    const tables = document.getElementsByTagName('table');
+    
+    inputContainer.classList.add('hidden');
+    pairingsContainer.classList.remove('hidden');
+
+    var rows = raw.split('\n');
+    var headers = rows[0].split(',');
+    const len = rows.length - 1;
+    var data = {};
+    
+    headers.forEach((h) => {
+        data[h] = [];
+    });
+
+    rows.slice(1).forEach((r) => {
+        r.split(',').forEach((x, i) => {
+            data[headers[i]].push(x);
+        })
+    })
+
+    const lens = [Math.ceil(len/3), Math.ceil(len/3), len - (2*Math.ceil(len/3))];
+    const lensPSA = [lens[0]];
+    lens.slice(1).forEach((x, i) => {lensPSA.push(lensPSA[i]+x)});
+
+    var i = 0;
+    var j = 0;
+    while (j < lensPSA[i]) {
+        board = data["#B"][j];
+        white = trim(data["White player"][j]);
+        black = trim(data["Black player"][j]);
+        console.log(board, white, black);
+        const tableRow = document.createElement("tr");
+        const boardCell = document.createElement("td"); boardCell.innerHTML = board; tableRow.appendChild(boardCell);
+        const whiteCell = document.createElement("td"); whiteCell.innerHTML = white; tableRow.appendChild(whiteCell);
+        const blackCell = document.createElement("td"); blackCell.innerHTML = black; tableRow.appendChild(blackCell);
+
+        tables[i].appendChild(tableRow);
+
+        j++;
+        if (j == lensPSA[i] && i < 2) i++;
+    }
+
+    console.log(data);
+    
 }
+
+function trim(str) {
+    if (str == "BYE") return str;
+    var arr = str.split(' ');
+    return `${arr[0]} ${arr[1][0]}.`;
+}
+
+// MAY NEED TO TRIM GOOGLE SHEETS NAMES BEFORE IF NAMES SHOW UP WRONG
