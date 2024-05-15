8a96194e631e0a5fe6863f5e145175e97c508233
mattshrew <mattshrewy@gmail.com>
2024-05-14 23:03:00 -0400

    Added FAQ section with animation and responsivity

--- script.js
+++ script.js
@@ -1,3 +1,95 @@
+var FAQs = [
+    {
+        question: "How much does it cost to attend the WRSSCC?",
+        answer: "The WRSSCC is completely free to attend!"
+    },
+    {
+        question: "Will there be food at the tournament?",
+        answer: "Yes! There will be free snacks provided throughout the tournament and pizza at the end, after the closing ceremony."
+    },
+    {
+        question: "What if I'm not good at chess?",
+        answer: "Don't worry! The WRSSCC welcomes all skill levels, beginners and advanced players alike."
+    },
+    {
+        question: "How long will the tournament run?",
+        answer: "The WRSSCC will start at 3:20 PM and end at around 5:30 PM."
+    },
+    {
+        question: "Will there be prizes for winners?",
+        answer: "Prizes will be awarded to the top 5 performing players. A raffle will also be hosted, so all players will have the chance to win leave with a prize!"
+    },
+    {
+        question: "What is the tournament format?",
+        answer: "Everyone will play 7 games. Each game will have a time limit of 5 minutes per player, with no increment. There will be an estimated 5 minute break in between each game."
+    },
+    {
+        question: "Can I leave in the middle of the tournament?",
+        answer: "It’d be great if you could stay the duration of the whole tournament, but if you must go in the middle, make sure to let one of the arbiters at the tournament know."
+    },
+    {
+        question: "What can I do if I arrive early?",
+        answer: "We'll have a BINGO challenge (with prizes for winners) for players who arrive before 3:20 PM. The BINGO board will be revealed on the day of the tournament."
+    },
+]
+
+var css = new function() {
+    function addStyle() {
+       let head = document.head;
+       let style = document.createElement("style");
+       head.appendChild(style);
+    }
+    this.insert = function(rule) {
+       if(document.styleSheets.length == 0) { addStyle(); }
+       let sheet = document.styleSheets[0];
+       let rules = sheet.cssRules;
+       sheet.insertRule(rule, rules.length);
+    }
+}
+ 
+
+function buildFAQs() {
+    const FAQcols = document.getElementsByClassName('faq-col');
+    var i = 0
+    FAQs.forEach((faq) => {
+        const accordion = document.createElement("div"); accordion.classList = "faq-accordion";
+        const symbol = document.createElement("img"); symbol.classList = "faq-symbol"; symbol.src = "img/faq.svg";
+        const question = document.createElement("div"); question.classList = "faq-question";
+        const questionTitle = document.createElement("h5"); questionTitle.classList = "faq-question-title";
+            questionTitle.innerHTML = faq.question;
+        const questionAnswer = document.createElement("p"); questionAnswer.classList = "faq-question-answer";
+            questionAnswer.innerHTML = faq.answer;
+        
+        question.appendChild(questionTitle);
+        question.appendChild(questionAnswer);
+        accordion.appendChild(symbol);
+        accordion.appendChild(question);
+        FAQcols[(i % 2)].appendChild(accordion);
+        
+        let height = questionAnswer.clientHeight;
+
+        accordion.id = `faq-${i}`;
+        accordion.classList.add("closed");
+
+        css.insert(`.faq-accordion.open#${accordion.id} .faq-question-answer {max-height: ${height}px !important}`);
+
+        i++;
+
+        [symbol, questionTitle].forEach((e) => {
+            e.addEventListener('click', function() {
+                console.log(accordion, height);
+                if (accordion.classList.contains('closed')) {
+                    accordion.classList.remove('closed');
+                    accordion.classList.add('open');
+                } else if (accordion.classList.contains('open')) {
+                    accordion.classList.remove('open');
+                    accordion.classList.add('closed');
+                }
+            });
+        });
+    });
+}
+
 document.addEventListener('DOMContentLoaded', function () {
     const leftArrow = document.getElementById('arrow-left');
     const rightArrow = document.getElementById('arrow-right');
@@ -27,11 +119,7 @@ document.addEventListener('DOMContentLoaded', function () {
         images[(mid+3) % images.length].classList = "hidden img-right";
         images[(mid-3 + images.length) % images.length].classList = "hidden img-left";
     }
-
-
-
-
-
+    
 
     const inputBox = document.getElementById('input-box');
     const fileInput = document.getElementById('input-file');
